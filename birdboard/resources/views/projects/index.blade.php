86110096ce84265d4acf3d02862e6c3ed2c77464
Justin Rodenbostel <justin@rodenbostel.com>
2019-02-11 23:10:42 -0600

    tailwind is awesome.

--- birdboard/resources/views/projects/index.blade.php
+++ birdboard/resources/views/projects/index.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center mb-3">
-        <a href="/projects/create">Create Project</a>
-    </div>
-    <div class="flex">
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-center w-full">
+            <h2 class="text-grey text-sm font-normal">My Projects</h2>
+            <a href="/projects/create" class="button">Add Project</a>
+        </div>
+    </header>
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
-            <div class="bg-white mr-4 rounded shadow w-1/3 p-5" style="height: 200px">
-                <h3 class="font-normal txt-xl mb-4 py-4">{{ $project->title }}</h3>
-                <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white mr-4 p-5 rounded-lg shadow" style="height: 200px">
+                    <a href="$project->path()" class="  no-underline text-black"><h3 class="font-normal txt-xl mb-4 -ml-5 mb-3 py-4 border-l-4 border-blue-light pl-4">{{ $project->title }}</h3></a>
+                    <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+                </div>
             </div>
         @empty
             <li>No projects yet.</li>
         @endforelse
-    </div>
+    </main>
 @endsection
